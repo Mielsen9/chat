ee327b1c86040b7085576c9d55e1f7da892cf38b
Mielsen_9 <142320197+Mielsen9@users.noreply.github.com>
2025-07-12 14:15:05 +0300

    add component

--- Client/src/components/pages/Main/Main.tsx
+++ Client/src/components/pages/Main/Main.tsx
@@ -1,6 +1,10 @@
 import React, {FC} from "react";
 import * as s from './Main.module.scss'
 import Chat from "@/components/organisms/Chat";
+import LogButton from "@/components/atoms/LogButton";
+import SearchInput from "@/components/atoms/SearchInput";
+import Avatar from "@/components/atoms/Avatar";
+import ChatView from "@/components/molecules/ChatView ";
 
 
 // Type
@@ -14,7 +18,27 @@ const Main: FC<Props> = (props) => {
 
   return (
     <div className={`${s.Main} ${s.wrapper}`}>
-      <Chat chatId={"1"}/>
+      <div className={s.MainContainer}>
+        <div className={s.SideBar}>
+          <div className={s.AvatarAndLogButton}>
+            <Avatar/>
+            <LogButton/>
+          </div>
+          <div className={s.SearchInput}>
+            <SearchInput/>
+          </div>
+          <div className={s.Chats}>
+            <h2>Chats</h2>
+            <ChatView/>
+            <ChatView/>
+            <ChatView/>
+            <ChatView/>
+          </div>
+        </div>
+        <div className={s.Chat}>
+          <Chat chatId={"1"}/>
+        </div>
+      </div>
     </div>
   )
 }
